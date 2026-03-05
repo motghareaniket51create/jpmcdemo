7c440d02e4d371fc52668c5b2c04484b3df36df5
motghareaniket51create <motghareaniket51@gmail.com>
2026-03-05 12:45:11 +0530

    newly created project

---
+++ src/main/java/jpmc/example/demo/JpmcdemoApplication.java
@@ -0,0 +1,13 @@
+package jpmc.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmcdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpmcdemoApplication.class, args);
+	}
+
+}
